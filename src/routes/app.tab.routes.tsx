645fcac790184bd48b69501c14bd7ed286aa22eb
CaioChristhian <caiochristhianz@gmail.com>
2024-04-06 16:08:50 -0300

    feat: estruturando rotas da aplicação e componentizando o app

---
+++ src/routes/app.tab.routes.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Login } from '../screens/Login';
+import { Text } from '../components/Text';
+import { PropsNavigationStack } from './models';
+import { Home } from '../screens/Home';
+
+const { Navigator, Screen } = createBottomTabNavigator<PropsNavigationStack>();
+const Stack = createNativeStackNavigator();
+
+interface PropsCustomTabBarLabel {
+	label: string;
+	focused: boolean;
+	color: string;
+}
+
+const CustomTabBarLabel = ({ label, focused, color }: PropsCustomTabBarLabel) => (
+	<>
+		<Text color={color} size={14}>{label}</Text>
+		<View style={{ borderBottomWidth: focused ? 2 : 0, borderBottomColor: '#D73035', width: 12, marginTop: 4 }}></View>
+	</>
+);
+
+function AppTabRoutes() {
+
+	return (
+		<Navigator
+			screenOptions={{
+				tabBarHideOnKeyboard: true,
+				headerShown: false,
+				tabBarActiveTintColor: '#D73035',
+				tabBarInactiveTintColor: '#666666',
+				tabBarShowLabel: true, // Exibir o texto do tabBarLabel
+				tabBarStyle: {
+					backgroundColor: '#FFFFFF',
+					minHeight: 72,
+					paddingBottom: 8,
+				},
+				tabBarLabelStyle: {
+					fontSize: 12,
+				},
+				tabBarIconStyle: {
+					marginBottom: -12
+				}
+			}}
+		>
+			<Screen
+				name='Home'
+				component={Home}
+				options={{
+
+					tabBarLabel: ({ focused, color }) => (
+						<CustomTabBarLabel color={color} label='Home' focused={focused} />
+					),
+				}}
+			/>
+		</Navigator>
+	);
+}
+
+export const AppRoutes = () => {
+
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false
+			}}
+			initialRouteName='Login'
+		>
+			<Stack.Screen
+				name='tab'
+				component={AppTabRoutes}
+			/>
+
+			<Stack.Screen
+				name='Login'
+				component={Login}
+			/>
+		</Stack.Navigator>
+	);
+};
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import * as S from './styles';
+import { Text } from '../../components/Text';
+
+export function Login(){
+	return (
+		<S.Container>
+			<Text>Login</Text>
+		</S.Container>
+	);
+}
